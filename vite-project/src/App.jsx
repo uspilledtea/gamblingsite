ccdf65b57fc4995e7a5adad797ddd7328b7933f1
uspilledtea <119764073+uspilledtea@users.noreply.github.com>
2024-10-12 16:54:47 +0000

    added mui

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Button from '@mui/material/Button';
 
 function App() {
   
   return (
-    <>
+    <><div >>
       <p>Test</p>
+      <Button>Uhhh</Button>
+      </div>
     </>
   )
 }
