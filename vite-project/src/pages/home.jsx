313c36064bacd48fdfed5962a875b72e791c858c
uspilledtea <119764073+uspilledtea@users.noreply.github.com>
2024-10-16 18:41:26 +0000

    update nav

--- vite-project/src/pages/home.jsx
+++ vite-project/src/pages/home.jsx
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 export function Home() {
     return(
         <>
-        <h1>homepage</h1>
+    
+        <div className="flex flex-col bg-slate-500 h-screen w-60 pt-16">
         <Link to="/">Home</Link>
         <Link to="/Games">Games</Link>
         <Link to="/Account">Account</Link>
+        </div>
 
         </>
     )
