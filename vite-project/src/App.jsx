c9d1ad96b6e79e04d8ba8e12be10fc7bb1f2a86c
uspilledtea <119764073+uspilledtea@users.noreply.github.com>
2024-10-16 17:26:41 +0000

    added tailwind

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import './ColorButton.jsx'
 import ColorButton from './ColorButton.jsx'
+import Grid from '@mui/material/Grid2'
 
 
 function App() {
@@ -8,7 +9,28 @@ function App() {
   return (
     <><div>
       <p>Test</p>
-      <ColorButton text="3"></ColorButton>
+      <p className ="text-3xl m-5 border-4 rounded-md">test</p>
+      <div id="mySidenav" className="sidenav">
+        <a href="javascript:void(0)" className="closebtn" onClick="closeNav()">&times;</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+
+      </div>
+
+      
+      <Grid container spacing={2}>
+        <Grid size={1}>
+          <ColorButton text="home"></ColorButton>    
+        </Grid>
+        <Grid size={1}>
+          <ColorButton text="games"></ColorButton>
+        </Grid>
+        <Grid size={1}>
+          <ColorButton text="account"></ColorButton>
+        </Grid>
+      </Grid>
       </div>
     </>
   )
