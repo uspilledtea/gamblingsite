223a38f57a994d5c74dc94f495927699cac9d921
uspilledtea <nguyenhbryan0818@gmail.com>
2024-10-16 19:27:58 -0400

    fix config

--- vite-project/src/pages/home.jsx
+++ vite-project/src/pages/home.jsx
@@ -5,7 +5,7 @@ export function Home() {
     return(
         <>
     
-        <div className="flex flex-col bg-slate-500 h-screen w-60 pt-16">
+        <div className="flex flex-col h-screen w-60 pt-16 pl-5 bg-orange-500">
         <Link to="/">Home</Link>
         <Link to="/Games">Games</Link>
         <Link to="/Account">Account</Link>
