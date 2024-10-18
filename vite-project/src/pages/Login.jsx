c8901c6587e92dac55ed27d47df3192865210639
uspilledtea <119764073+uspilledtea@users.noreply.github.com>
2024-10-18 18:12:50 +0000

    added login page

---
+++ vite-project/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+function Login() {
+    return (
+        <>
+        <p>Login</p>
+        </>
+    )
+}
+
+export default Login
